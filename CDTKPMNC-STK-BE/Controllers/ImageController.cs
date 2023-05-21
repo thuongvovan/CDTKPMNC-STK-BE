978f5b50faf0c41b18c4848d0bba140ca2767a52
Vo Van Thuong <vovanthuong@outlook.com.vn>
2023-05-21 09:39:15 +0700

    Add random images

---
+++ CDTKPMNC-STK-BE/Controllers/ImageController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CDTKPMNC_STK_BE.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _environment;
+        private readonly string _contentRootPath;
+
+        public ImageController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+            _contentRootPath = _environment.ContentRootPath;
+        }
+
+        // GET: <ImageController>
+        [HttpGet("StoreBanner")]
+        public PhysicalFileResult GetStoreBanner()
+        {
+            var folderPath = Path.Combine(_contentRootPath, "Images", "StoreBanners");
+            string[] files = Directory.GetFiles(folderPath);
+            var random = new Random();
+            string randomFile = files[random.Next(files.Length)];
+            return PhysicalFile(randomFile, "image/jpeg");
+        }
+
+        // GET: <ImageController>
+        [HttpGet("ProductItem")]
+        public PhysicalFileResult GetProductItemImage()
+        {
+            var folderPath = Path.Combine(_contentRootPath, "Images", "ProductItems");
+            string[] files = Directory.GetFiles(folderPath);
+            var random = new Random();
+            string randomFile = files[random.Next(files.Length)];
+            return PhysicalFile(randomFile, "image/jpeg");
+        }
+    }
+}
